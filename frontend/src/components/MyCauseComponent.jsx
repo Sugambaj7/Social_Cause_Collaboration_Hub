9cb1752ae4ef529e6940514c85298ffcc2896087
Sugambaj7 <sugambajrabajracharya@kbc.edu.np>
2025-07-02 20:31:50 +0545

    frontend validation while adding cause compled

--- frontend/src/components/MyCauseComponent.jsx
+++ frontend/src/components/MyCauseComponent.jsx
@@ -6,22 +6,25 @@ export default function MyCauseComponent() {
   const [addCausePopup, setAddCausePopup] = useState(false);
   const [causeName, setCauseName] = useState("");
   const [placeName, setPlaceName] = useState("");
-  const [time, setTime] = useState("");
   const [causeDescription, setCauseDescription] = useState("");
-  const [collaborationDeadline, setCollaborationDeadline] = useState("");
+  const [collaborationApplicationDeadline, setCollaborationApplicationDeadline] = useState("");
+  const [time, setTime] = useState("");
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
   const [error, setError] = useState("");
 
   //getting current date
   const currentDate = new Date();
+  console.log("Current Date:", currentDate);
+  const formattedCurrentDate = currentDate.toISOString().split('T')[0];
+  console.log("Formatted Current Date:", formattedCurrentDate);
   
 
   const validateAddForm = () => {
     if (causeName === "") {
       setError("Cause Name is not supposed to be empty");
       return false;
-    } else if (causeName.length < 8 && causeName.length > 50) {
+    } else if (causeName.length < 8 || causeName.length > 50) {
       setError(
         "Cause Name should be at least 8 characters long and maximum 50 characters long"
       );
@@ -34,7 +37,7 @@ export default function MyCauseComponent() {
     } else if (placeName === "") {
       setError("Place Name is not supposed to be empty");
       return false;
-    } else if (placeName.length < 2 && placeName.length > 25) {
+    } else if (placeName.length < 2 || placeName.length > 25) {
       setError(
         "Place Name should be at least 2 characters long and maximum 25 characters long"
       );
@@ -43,10 +46,11 @@ export default function MyCauseComponent() {
       setError(
         "Place Name should not contain special characters, numbers except spaces and comma "
       );
+      return false;
     } else if (causeDescription === "") {
       setError("Cause Description is not supposed to be empty");
       return false;
-    } else if (causeDescription.length < 10 && causeDescription.length > 200) {
+    } else if (causeDescription.length < 10 || causeDescription.length > 200) {
       setError(
         "Cause Description should be at least 10 characters long and maximum 200 characters long"
       );
@@ -54,27 +58,54 @@ export default function MyCauseComponent() {
     } else if (!/^[A-Za-z0-9.,!?'"()&%\- ]+$/.test(causeDescription)) {
       setError("Invalid Cause Description");
       return false;
-    } else if (collaborationDeadline === "") {
+    } else if (collaborationApplicationDeadline === "") {
       setError("Collaboration Deadline is not supposed to be empty");
       return false;
-    } else {
-      console.log("collaborationDeadline", collaborationDeadline);
+    } 
+    else if(collaborationApplicationDeadline < formattedCurrentDate) {
+      setError("Application Deadline cannot be in the past");
+      return false;
+    }
+    else if(time === "") {
+      setError("Time is not supposed to be empty");
+      return false;
+    }
+     else if (startDate === "") {
+      setError("Start Date is not supposed to be empty");
+      return false;
+    }
+    else if(startDate < formattedCurrentDate ) {
+      setError("Start Date cannot be in the past");
+      return false;
+    }
+    else if(startDate < collaborationApplicationDeadline){
+      setError("Start Date cannot be before the Application for collaboration Deadline");
+      return false;
+    }
+    else if (endDate === "") {
+      setError("End Date is not supposed to be empty");
+      return false;
+    }
+    else if (endDate < startDate) {
+      setError("End Date cannot be before Start Date");
+      return false;
+    }
+    else {
       setError("");
       return true;
-    }
   };
+};
 
   const handleAddCause = (e) => {
     validateAddForm();
     console.log("Cause Name:", causeName);
     console.log("Place Name:", placeName);
     console.log("Cause Description:", causeDescription);
-    console.log("Time:", time);
-    console.log("Collaboration Deadline:", collaborationDeadline);
+    console.log("Collaboration Deadline:", collaborationApplicationDeadline);
     console.log("Start Date:", startDate);
     console.log("End Date:", endDate);
     e.preventDefault();
-  };
+  }
 
   return (
     <div>
@@ -82,7 +113,7 @@ export default function MyCauseComponent() {
         <h3 className="text-2xl font-semibold mt-12 mb-6 tracking-wider">
           My Causes
         </h3>
-        {error ? <p className="text-red-600">{error}</p> : ""}
+       
 
         <button
           className="bg-custom_blue text-white px-4 py-2 rounded-md absolute right-0 top-[40%]"
@@ -168,6 +199,7 @@ export default function MyCauseComponent() {
                     className="flex flex-col gap-4"
                     onSubmit={handleAddCause}
                   >
+                     {error ? <p className="text-red-600">{error}</p> : ""}
                     <input
                       type="text"
                       placeholder="Please Enter Cause Name"
@@ -193,7 +225,7 @@ export default function MyCauseComponent() {
                       type="date"
                       placeholder="Please Enter Deadline For Collaboration"
                       className="px-4 py-2 border"
-                      onChange={(e) => setCollaborationDeadline(e.target.value)}
+                      onChange={(e) => setCollaborationApplicationDeadline(e.target.value)}
                     />
                     <input
                       type="time"
